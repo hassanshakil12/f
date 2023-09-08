0c6de77369e0626c7af5a6223d3fd29cd8c318d3
Muhammad Hassan <hassanshakil12@gmail.com>
2023-09-09 04:26:37 +0500

    Initialization

--- components/Home.js
+++ components/Home.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Header from "@/components/Header"
+import page from "@/app/page"
 
 const Home = () =>{
     return(
-        <></>
+        <>
+        <Header/>
+        <h1>This is Home Page</h1>
+        </>
     )
 }
 
